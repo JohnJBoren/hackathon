61b4738d73a0f84b899c2ff05478da055c861b9c
John J Boren <jboren@alaska.edu>
2019-10-13 14:29:33 -0800

    Insta
    loading some

---
+++ eventhandler.py
@@ -0,0 +1,73 @@
+
+from watchdog.events import FileSystemEventHandler
+
+from post import Post
+from setup_ import POST_DAYS, POST_LIST, PASSWORD, USERNAME, DATES_OF_POSTS, DAYS_MAP, POST_HOUR
+import os 
+import json
+import datetime
+from datetime import timedelta  
+import tkinter as tk
+from tkinter import *
+import time
+
+path = os.getcwd() + '/post'
+class MyHandler(FileSystemEventHandler):
+    i = 1
+    new_description = ''
+    def on_modified(self, event):
+        self.i = 1
+        folder_path = os.getcwd() + '/production_ready_posts'
+        list_of_file_names = []
+        for filename in os.listdir(path): 
+            new_name = "post_" + str(self.i) + ".jpg"
+            exists = True
+            while exists:
+                file_exists = os.path.isfile(os.getcwd() +'/' + new_name)
+                if file_exists:
+                    DATES_OF_POSTS[new_name] = POST_LIST.posts[self.i-1]
+                    self.i += 1
+                    new_name = "post_" + str(self.i) + ".jpg"
+                else:
+                    exists = False
+
+            new_name = folder_path + "/" + new_name
+            src = path + "/" + filename
+            self.description_added('', src, new_name)
+
+
+    def description_added(self, description, src, new_name):
+            os.rename(src, new_name) 
+            self.i += 1
+            date_of_post = self.get_next_available_date()
+            new_post = Post(description, 'post_' + str(self.i - 1) + ".jpg", date_of_post, False)
+            DATES_OF_POSTS['post_' + str(self.i - 1) + ".jpg"] = new_post
+            POST_LIST.posts.append(new_post)
+            self.save_post()
+            try:
+                POST_LIST.initialize_me()
+            except Exception as e:
+                print('no json')
+
+    def save_post(self):
+        with open('posts.json', 'w') as json_file:  
+            json.dump(POST_LIST.serialize(), json_file)
+
+    
+    def get_next_available_date(self):
+        return_date = datetime.date.today()
+        try:
+            post = DATES_OF_POSTS['post_' + str(len(POST_LIST.posts)) + ".jpg"]
+            return_date = post.post_date + timedelta(days=1)
+        except Exception as e:
+            print("exception")
+
+        exists = True
+        while exists:
+            if DAYS_MAP[return_date.weekday()] not in POST_DAYS:
+                return_date = return_date + timedelta(days=1)
+                print("EXISTS") 
+            else:
+                exists = False
+        return return_date
+        
---
+++ setup_.py
@@ -0,0 +1,19 @@
+from post_list import PostList
+
+POST_HOUR = 13
+POST_DAYS = ['saturday', 'sunday', 'thursday', 'wednesday']
+POST_LIST = PostList([])
+DATES_OF_POSTS = {}
+
+USERNAME = "tensorrandos"
+PASSWORD = "dp&*eAcpeQ?/7#9"
+
+DAYS_MAP = {
+    0: 'monday',
+    1: 'tuesday',
+    2: 'wednesday',
+    3: 'thursday',
+    4: 'friday',
+    5: 'saturday',
+    6 :'sunday',
+}
